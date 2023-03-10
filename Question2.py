f38fb5b0c901a61648e98b2fabb34005dc54ad57
RupertGithub <AWS3DEMO@gmail.com>
2023-03-10 15:16:13 +0800

    add group_by_key

--- Question2.py
+++ Question2.py
@@ -7,5 +7,25 @@ def count(input):
             word_count_dict.update({c:1})
     return word_count_dict
 
-input1 = ['a','b','c','a','c','a','x']
-print(count(input1))
+input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'x'] 
+print(count(input1)) # should print {'a': 3, 'b': 1, 'c': 2, 'x': 1} 
+
+
+
+def group_by_key(input): 
+    word_count_dict = {}
+    for word_dict in input:
+        if word_dict['key'] in word_count_dict:
+            word_count_dict[word_dict['key']]=word_count_dict[word_dict['key']]+word_dict['value']
+        else:
+            word_count_dict.update({word_dict['key']:word_dict['value']})
+    return word_count_dict
+
+input2 = [ 
+{'key': 'a', 'value': 3}, 
+{'key': 'b', 'value': 1}, 
+{'key': 'c', 'value': 2}, 
+{'key': 'a', 'value': 3}, 
+{'key': 'c', 'value': 5} 
+] 
+print(group_by_key(input2)) # should print {‘a’: 6, ‘b’: 1, ‘c’: 7}
